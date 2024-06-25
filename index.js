c41bcca5a744cd89b05c2bbc4b4c83043180c20c
Ben Thomas <benjamint08@gmail.com>
2024-06-25 22:54:05 +0100

    This works so much better now

--- index.js
+++ index.js
@@ -90,25 +90,83 @@ const PORT = process.env.PORT || 4040;
 // });
 
 // read /etc/caddy/Caddyfile and send it to the client
-fs.readFile('/etc/caddy/Caddyfile', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-    // start the server and caddy
-    child_process.exec('caddy run --config /etc/caddy/Caddyfile', (err, stdout, stderr) => {
-        if (err) {
-            console.error(err);
-            return;
+
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+let uri = process.env.MONGODB_URI;
+if (!uri) uri = 'mongodb://vdb:27017';
+const client = new MongoClient(uri);
+
+async function startCaddy() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const resources = client.db('vessyl').collection('resources');
+        const allResources = await resources.find({}).toArray();
+        let config = [];
+        allResources.forEach(resource => {
+            config.push({
+                domain: resource.domain,
+                port: resource.ports[0].split(':')[0]
+            });
+        });
+
+        let gateway = '';
+
+        try {
+            gateway = child_process.execSync("ip route | grep default | awk '{print $3}'").toString().trim();
+        } catch (e) {
+            console.error('Failed to get gateway IP');
         }
-        console.log(stdout);
-        console.error(stderr);
-    });
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-});
+
+        // write to /etc/caddy/Caddyfile
+        let caddyConfig = `
+        http:// {
+            respond "VSYL-001 Resource not found" 404
+        }
+        
+        https:// {
+            respond "VSYL-001 Resource not found" 404
+        }
+        `
+        config.forEach(resource => {
+            let resourceConfig = `
+            ${resource.domain} {
+                reverse_proxy ${gateway}:${resource.port}
+            
+                handle_errors {
+                    @proxy_failed {
+                        expression {http.error.status_code} == 502 || {http.error.status_code} == 503 || {http.error.status_code} == 504
+                    }
+                    respond @proxy_failed "VSYL-002 Resource unavailable" 503
+                }
+            }
+            `;
+            resourceConfig = resourceConfig.trim();
+            caddyConfig += resourceConfig + '\n';
+        });
+
+        const configFilePath = '/etc/caddy/Caddyfile';
+        fs.writeFile(configFilePath, caddyConfig, (err) => {
+            if (err) {
+                console.error('Failed to write Caddy config file');
+            }
+
+            exec('caddy run --config /etc/caddy/Caddyfile', (err, stdout, stderr) => {
+                if (err) {
+                    console.error('Failed to reload Caddy', stderr);
+                }
+
+                console.log('Caddy configuration added and reloaded successfully');
+            });
+        });
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+startCaddy();
 
 process.on('SIGTERM', () => {
     console.log('Received SIGTERM. Shutting down gracefully...');
